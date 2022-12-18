1318f0644122fba650ebe70ab545a790d3fcdb74
AlanPan0131 <panalan0131@gmail.com>
2022-12-18 16:38:29 +0800

    update

---
+++ README.md
@@ -0,0 +1,12 @@
+# 🔥Origin 起源劇團🔥 官方網站
+
+## 架構
+```
+index 首頁
+ ├─ about 關於我們
+ ├─ event 活動行程
+ │   └─ 活動行程內容
+ ├─ blog BLOG
+ │   └─ BLOG內容
+ └─ contact 聯絡我們
+```
--- public/js/firebase/firebase-storage.js
+++ public/js/firebase/firebase-storage.js
@@ -1,7 +1,11 @@
-import { getStorage, ref, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-storage.js';
+import { getStorage, ref, getDownloadURL, uploadBytes } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-storage.js';
 
 function getURL(storage, path) {
     return getDownloadURL(ref(storage, path));
 };
 
-export { getStorage, getURL };
+function uploadBytes(storage, path, file) {
+    return uploadBytes(ref(storage, path), file);
+}
+
+export { getStorage, getURL, uploadBytes };
