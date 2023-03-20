640560c11f7858a59a1adab889dc6e7fd3e1bb49
AlanPan0131 <panalan0131@gmail.com>
2023-03-21 00:14:59 +0800

    update

--- src/main.js
+++ src/main.js
@@ -7,12 +7,15 @@ import { firebaseApp, firebaseAnalytics } from './firebase'
 
 import VueSplide from '@splidejs/vue-splide';
 
-const app = createApp(App).use(VueFire, {
+const app = createApp(App);
+
+app.use(VueFire, {
     // imported above but could also just be created here
     firebaseApp,
     modules: [
         VueFireAuth(),
     ],
-}).use(router).use(VueSplide).mount('#app')
+}).use(router).use(VueSplide).mount('#app');
 
 app.config.globalProperties.$analytics = firebaseAnalytics;
+console.log(app.config)
