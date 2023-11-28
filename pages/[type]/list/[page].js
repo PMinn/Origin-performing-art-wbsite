3dbcdb539c3236ad80b507ff5da1890a1d9d6b6a
PMinn <40393799+PMinn@users.noreply.github.com>
2023-11-29 00:44:35 +0800

    test

--- pages/[type]/list/[page].js
+++ pages/[type]/list/[page].js
@@ -86,7 +86,7 @@ export default function BlogList({ data, title, description, type, page, headlin
                         </div>
                         :
                         <></>
-                } */}
+                } */} 
             </div>
         </Layout>
     )
