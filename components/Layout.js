1724460c8a48550936ac622239db22a8bdf377ba
PMinn <40393799+PMinn@users.noreply.github.com>
2024-02-20 08:46:54 +0800

    Update layout

--- components/Layout.js
+++ components/Layout.js
@@ -5,10 +5,11 @@ export default function Layout({ children, loading }) {
     const [isLoading, setIsLoading] = useState(true);
     return (
         <>
-            <main onLoad={() => { if (typeof loading == 'undefined') setIsLoading(false); }}>
+            {/* <main onLoad={() => { if (typeof loading == 'undefined') setIsLoading(false); }}>
                 <LoadingComponent isLoading={typeof loading == 'undefined' ? isLoading : loading}></LoadingComponent>
                 {children}
-            </main>
+            </main> */}
+            <main>{children}</main>
         </>
     )
 }
